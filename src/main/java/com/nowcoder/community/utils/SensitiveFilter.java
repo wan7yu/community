01d12978964026c7c5a6380bef7d1aa14162cb53
Wang Peng <2077692972@qq.com>
2024-06-30 15:34:57 +0800

    增加敏感词过滤器(基于前缀树)

---
+++ src/main/java/com/nowcoder/community/utils/SensitiveFilter.java
@@ -0,0 +1,162 @@
+package com.nowcoder.community.utils;
+
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class SensitiveFilter {
+
+    // 日志
+    private static final Logger logger = LoggerFactory.getLogger(SensitiveFilter.class);
+
+    // 替换符号
+    private static final String REPLACEMENT = "***";
+
+    // 根节点
+    private TrieNode rootNode = new TrieNode();
+
+    @PostConstruct
+    public void init() {
+        try (
+                        InputStream is = this.getClass().getClassLoader().getResourceAsStream("sensitive-words.txt");
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                ) {
+            String keyword;
+            while ((keyword = br.readLine()) != null) {
+                // 添加到前缀树
+                this.addKeyword(keyword);
+            }
+        } catch (IOException e) {
+            logger.error("加载敏感词文件失败：" + e.getMessage());
+        }
+    }
+
+    // 将一个敏感词添加到前缀树中
+    private void addKeyword(String keyword) {
+        TrieNode tempNode = rootNode;
+
+        for (int i = 0; i < keyword.length(); i++) {
+            char c = keyword.charAt(i);
+            TrieNode subNode = tempNode.getSubNode(c);
+
+            if (subNode == null) {
+                // 说明没有节点，那么初始化子节点
+                subNode = new TrieNode();
+                tempNode.addSubNode(c, subNode);
+            }
+
+            // 指向子节点
+            tempNode = subNode;
+            // 设置结束标识
+            if (i == keyword.length() - 1) {
+                tempNode.setKeywordEnd(true);
+            }
+        }
+    }
+
+    /*
+      * 过滤敏感词
+      * @param text: 待过滤的文本
+      * @return String: 过滤后的文本
+     */
+    public String filter(String text) {
+        if (StringUtils.isBlank(text)) {
+            return null;
+        }
+
+        TrieNode tempNode = rootNode;
+        // 开始指针，和结尾指针
+        int begin = 0, position = 0;
+        // 拼接字符，放回结果
+        StringBuilder sb = new StringBuilder();
+
+        while (position < text.length()) {
+            char c = text.charAt(position);
+
+            // 跳过符号
+            if (isSymbol(c)) {
+                // 若指针1处于根节点，将此符号计入结果中，让指针2向下走一步
+                if (tempNode == rootNode) {
+                    sb.append(c);
+                    begin++;
+                }
+                // 无论符号在开头或者中间，指针都需要往后走
+                position++;
+                continue;
+            }
+
+            // 不是符号，检查下级节点
+            tempNode = tempNode.getSubNode(c);
+            if (tempNode == null) {
+                // 说明以begin开头的字符串不是敏感词
+                sb.append(text.charAt(begin));
+                // 进入下一个位置
+                position = ++begin;
+                // 重新指向根节点, 寻找下一个可能的敏感词
+                tempNode = rootNode;
+            } else if (tempNode.isKeywordEnd()){
+                // 发现敏感词，将begin到position之间的字符串替换掉
+                sb.append(REPLACEMENT);
+                // 进入下一个位置
+                begin = ++position;
+                // 重新指向根节点
+                tempNode = rootNode;
+            } else {
+                // 检查下一个字符
+                position++;
+            }
+        }
+
+        // 将最后一批字符计入结果
+        sb.append(text.substring(begin));
+
+        return sb.toString();
+    }
+
+    // 判断是否为符号
+    private boolean isSymbol(Character c) {
+        // isAsciiAlphanumeric是判断是否为特殊字符，是则放回false
+        // 0x2E80 ~ 0x9FFF 是东亚文字范围
+        return !CharUtils.isAsciiAlphanumeric(c) && (c < 0x2E80 || c > 0x9FFF);
+    }
+
+    // 前缀树节点
+    private class TrieNode {
+        // 标识当前词结尾的单词是否是敏感词
+        private boolean isKeywordEnd = false;
+
+        // 子节点, key是下级字符，value是下级节点
+        private Map<Character, TrieNode> subNodes = new HashMap<>();
+
+        public boolean isKeywordEnd() {
+            return isKeywordEnd;
+        }
+
+        public void setKeywordEnd(boolean keywordEnd) {
+            isKeywordEnd = keywordEnd;
+        }
+
+        // 添加子节点
+        public void addSubNode(Character c, TrieNode node) {
+            subNodes.put(c, node);
+        }
+
+        // 获取子节点
+        public TrieNode getSubNode(Character c) {
+            return subNodes.get(c);
+        }
+    }
+
+
+}
